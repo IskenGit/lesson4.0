f9728c00145670baa3ed73efe86f86e86a3f5144
IskenGit <isken1990@gmail.com>
2024-06-05 15:40:07 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-print("Я конфликтую")
-for i in range(8):
-    print("слово")
-a, b = 9, 8
-if a > b:
-    print("a больше чем b")
-elif b > a:
-    print("b больше чем a")
-else:
-    print("a равно b")
+c = 38
+d = 15
+e = 1598
 
